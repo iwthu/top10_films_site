89d9882203972abae37d84037537de2cf41279cd
iwthu <102432185+iwthu@users.noreply.github.com>
2023-03-24 13:45:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class RateMovieForm(FlaskForm):
     rating = StringField("Your Rating Out of 10 e.g. 7.5", validators=[DataRequired()])
     review = StringField("Your Review", validators=[DataRequired()])
     submit = SubmitField("Done")
-
+ 
 
 @app.route("/")
 def home():
